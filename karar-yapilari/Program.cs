1c3b558e5cc0f46478d9344049fb7723fb80cc45
M Emin Akyüz <akyuzz.eminn@gmail.com>
2022-02-13 17:33:46 +0300

    ..

---
+++ karar-yapilari/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace karar_yapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression -- kontrol edilmek istenen durum
+            switch (month)
+            {
+                
+                case 1:
+                    Console.WriteLine("Ocak");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Şubat");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Mart");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Nisan");
+                    break;
+
+
+                default:
+                break;
+            }
+            switch (month)
+            {
+                case 12: 
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimi");
+                    break;
+
+                case 3: 
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimi");
+                    break;
+                
+
+                default:
+                break;
+            }
+        }
+    }
+}
