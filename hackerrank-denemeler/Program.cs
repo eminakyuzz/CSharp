496d9349d76950f4f2964aa64c5f2dfb5fdc2110
M Emin Akyüz <akyuzz.eminn@gmail.com>
2022-02-27 14:15:26 +0300

    .....

---
+++ hackerrank-denemeler/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace hackerrank_denemeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir dizideki en çok tekrar eden sayıyının tekrar sayısını yazdırma
+            // int[] dizi = { 11, 11, 11, 10, 1 };
+
+            // //Array.Sort(dizi);
+
+
+            // int count = 0;
+            // int max = 0;
+            // foreach (var item in dizi)
+            // {
+            //     int num = item;
+            //     if (num > max)
+            //     {
+            //         max = num;
+            //         count = 1;
+            //     }
+            //     else if (max == num)
+            //     {
+            //         count++;
+            //     }
+
+            // }
+            // Console.WriteLine(count);
+
+            // //bir dizideki elemanları toplamak
+            // System.Console.WriteLine("*********dizi eleman toplamları********");
+
+            // int[] dizi2 = new int[] { 10, 20, 10 };
+            // int toplam = 0;
+            // foreach (var i in dizi2)
+            // {
+            //     toplam += i;
+            // }
+            // Console.WriteLine(toplam);
+
+            // int aCount = 0;
+            // int bCount = 0;
+
+            // int[] a = new int[]{1,65,2,19};
+            // int[] b = new int[]{4,18,6,9};
+
+
+            // foreach (var item1 in a)
+            // {
+            //     foreach (var item2 in b)
+            //     {
+            //         Console.WriteLine(item2);
+                    
+                    
+            //             if (item1>item2)
+            //             {
+            //                 aCount++;
+            //             }
+            //             if (item1<item2)
+            //             {
+            //                 bCount++;
+            //             }
+                        
+            //     }
+
+            // }
+            // Console.Write( aCount/a.Length + " "+ bCount/b.Length);
+
+
+
+        //     int alice=0,bob=0;
+        // for(int i=0;i<a.Count;i++)
+        // {
+        //     if(a.ElementAt(i)>b.ElementAt(i))
+        //     {
+        //         alice=alice+1;
+        //     }
+        //     else if(a.ElementAt(i)<b.ElementAt(i))
+        //     {
+        //         bob=bob+1;
+        //     }
+        // }
+        // return new List<int> {alice,bob};
+            
+
+
+        }
+    }
+}
