f4426808064bfb9aad91ae37072b90c17b4770ea
M Emin Akyüz <akyuzz.eminn@gmail.com>
2022-03-06 16:56:18 +0300

    last commit

--- metot-overloading/Program.cs
+++ metot-overloading/Program.cs
@@ -7,20 +7,49 @@ namespace metot_overloading
         static void Main(string[] args)
         {
             //out parametreler
-            string sayi = "999";
+            string sayi ="989";
+            int outSayi;
 
-            int outSayi ;
-            bool sonuc = int.TryParse(sayi,out outSayi);
-
+            bool sonuc = int.TryParse(sayi, out outSayi);
             if (sonuc)
             {
-                Console.WriteLine("Başarılı....");
-                Console.Write(outSayi);
+                Console.WriteLine("Başarılı ");
+                Console.WriteLine(outSayi);
             }
             else
             {
-                Console.WriteLine("Başarısız.....");
+                Console.WriteLine("Başarısız!!!!!");
             }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(2,5,out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            int ifade = 999;
+            instance.EkranaYazdir(ifade);
+
+            instance.EkranaYazdir("emin","arslan");
+
+            //Metot imzası
+            //metot adı + parametre sayısı + parametre tipi
+
+        }
+    }
+    class Metotlar
+    {
+        public  void Topla(int a, int b, out int toplam)
+        {
+            toplam = a+b;
+        }
+
+        public void EkranaYazdir(string veri){
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri){
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(string veri1, string veri2){
+            Console.WriteLine(veri1+" "+veri2);
         }
     }
 }
