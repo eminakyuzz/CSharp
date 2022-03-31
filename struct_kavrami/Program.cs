8227b0013706f5e2ba0cd985a10a68fc8b2dd231
M Emin Akyüz <akyuzz.eminn@gmail.com>
2022-03-31 16:56:14 +0300

    .

---
+++ struct_kavrami/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace struct_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdörtgen dikdörtgen = new Dikdörtgen();
+            // dikdörtgen.KisaKenar = 3 ;
+            // dikdörtgen.UzunKenar = 5 ;
+            System.Console.WriteLine("class alan hesabı :{0}", dikdörtgen.AlanHesapla());
+
+            Dikdörtgen_Struct dikdörtgen_struct = new Dikdörtgen_Struct(3,4); //struct newlenebilir ve böyle de kullanılabilir.
+            
+            System.Console.WriteLine("struct alan hesabı :{0}",dikdörtgen_struct.AlanHesapla());           
+        }
+    }
+    class Dikdörtgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        // public Dikdörtgen()
+        // {
+        //     KisaKenar = 3 ;
+        //     UzunKenar = 4 ;
+        // }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+    struct Dikdörtgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdörtgen_Struct(int kisaKenar, int uzunKenar) //struct lar parametreli construct  yapabilir.
+        {
+            
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar ;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+
+
+    }
+}
